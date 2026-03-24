6fbd4e74cf9ec0a33a7a7a543f0b1b3bb433f916
SamuelES26 <samuelmariano@discente.ufg.br>
2026-03-23 21:22:29 -0300

    Update Aula.go

--- Aula.go
+++ Aula.go
@@ -6,7 +6,9 @@ func main() {
 	var ld1, ld2, ld3 int64
 	fmt.Println("Coloque os dados:")
 	fmt.Scan(&ld1, &ld2, &ld3)
-	if ld1 == ld2 && ld2 == ld3 {
+	if ld1 >= (ld2+ld3) || ld2 >= (ld1+ld3) || ld3 >= (ld1+ld2) {
+		fmt.Println("Triangulo Invalido")
+	} else if ld1 == ld2 && ld2 == ld3 {
 		fmt.Println("Equilátero")
 	} else if ld1 == ld2 || ld1 == ld3 || ld3 == ld2 {
 		fmt.Println("Isosceles")
